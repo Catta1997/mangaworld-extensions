96f5c19ac684d84a76e9ab148beef0de9065b626
Sinon <34333654+Catta1997@users.noreply.github.com>
2026-05-07 21:05:41 +0200

    chore: Refactor advanced search form and bump deps

--- src/generic/forms/search.ts
+++ src/generic/forms/search.ts
@@ -3,7 +3,6 @@
 
 import {
   AdvancedSearchForm,
-  type FormItemElement,
   type FormSectionElement,
   type SearchQuery,
   Section,
@@ -41,77 +40,65 @@ export class MangaWorldAdvancedSearchForm extends AdvancedSearchForm {
   }
   override getSections(): FormSectionElement<unknown>[] {
     return [
-      Section("type", this.getGenreFilter()),
-      Section("genres", this.getTypeFilter()),
-      Section("year", this.getYearFilter()),
-      Section("status", this.getStatusFilter()),
-    ];
-  }
-  getGenreFilter(): FormItemElement<unknown>[] {
-    return [
-      TriStateSelectRow("genres", {
-        title: "Generi",
-        value: this.searchMetadata.genres ?? {},
-        layout: "list",
-        items: filter.getGenreFilter().map((x) => ({ id: x.id, title: x.value })),
-        allowExclusion: true,
-        allowEmptySelection: true,
-        onValueChange: Application.Selector(
-          this as MangaWorldAdvancedSearchForm,
-          "handleGenreChange",
-        ),
-      }),
+      Section("type", [
+        TriStateSelectRow("type", {
+          title: "Tipologia",
+          value: this.searchMetadata.type ?? {},
+          layout: "list",
+          allowExclusion: true,
+          allowEmptySelection: true,
+          items: filter.getMangaTypeFilter().map((x) => ({ id: x.id, title: x.value })),
+          onValueChange: Application.Selector(
+            this as MangaWorldAdvancedSearchForm,
+            "handleTypeChange",
+          ),
+        }),
+      ]),
+      Section("genres", [
+        TriStateSelectRow("genres", {
+          title: "Generi",
+          value: this.searchMetadata.genres ?? {},
+          layout: "list",
+          items: filter.getGenreFilter().map((x) => ({ id: x.id, title: x.value })),
+          allowExclusion: true,
+          allowEmptySelection: true,
+          onValueChange: Application.Selector(
+            this as MangaWorldAdvancedSearchForm,
+            "handleGenreChange",
+          ),
+        }),
+      ]),
+      Section("year", [
+        StepperRow(`year`, {
+          title: "Anno",
+          value: this.searchMetadata.year ?? 0,
+          minValue: 1990,
+          maxValue: new Date().getFullYear(),
+          stepValue: 1,
+          loopOver: false,
+          onValueChange: Application.Selector(
+            this as MangaWorldAdvancedSearchForm,
+            "handleYearChange",
+          ),
+        }),
+      ]),
+      Section("status", [
+        SelectRow("status", {
+          title: "Stato",
+          subtitle: "Seleziona lo stato",
+          value: this.searchMetadata.status ?? [],
+          minItemCount: 0,
+          maxItemCount: 1,
+          options: filter.getStatusFilter().map((x) => ({ id: x.id, title: x.value })),
+          onValueChange: Application.Selector(
+            this as MangaWorldAdvancedSearchForm,
+            "handleStatusChange",
+          ),
+        }),
+      ]),
     ];
   }
 
-  getTypeFilter(): FormItemElement<unknown>[] {
-    return [
-      TriStateSelectRow("type", {
-        title: "Tipologia",
-        value: this.searchMetadata.type ?? {},
-        layout: "list",
-        allowExclusion: true,
-        allowEmptySelection: true,
-        items: filter.getMangaTypeFilter().map((x) => ({ id: x.id, title: x.value })),
-        onValueChange: Application.Selector(
-          this as MangaWorldAdvancedSearchForm,
-          "handleTypeChange",
-        ),
-      }),
-    ];
-  }
-  getStatusFilter(): FormItemElement<unknown>[] {
-    return [
-      SelectRow("status", {
-        title: "Stato",
-        subtitle: "Seleziona lo stato",
-        value: this.searchMetadata.status ?? [],
-        minItemCount: 0,
-        maxItemCount: 1,
-        options: filter.getStatusFilter().map((x) => ({ id: x.id, title: x.value })),
-        onValueChange: Application.Selector(
-          this as MangaWorldAdvancedSearchForm,
-          "handleStatusChange",
-        ),
-      }),
-    ];
-  }
-  getYearFilter(): FormItemElement<unknown>[] {
-    return [
-      StepperRow(`year`, {
-        title: "Year",
-        value: this.searchMetadata.year ?? 0,
-        minValue: 0,
-        maxValue: 5,
-        stepValue: 1,
-        loopOver: false,
-        onValueChange: Application.Selector(
-          this as MangaWorldAdvancedSearchForm,
-          "handleYearChange",
-        ),
-      }),
-    ];
-  }
   async handleTypeChange(value: Record<string, "included" | "excluded">): Promise<void> {
     this.searchMetadata.type = value;
   }
